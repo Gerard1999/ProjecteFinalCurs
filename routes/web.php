64bd99d7b8f68add7a217baf399ada7146324b60
Gerard López <46751836+Gerard1999@users.noreply.github.com>
2021-04-26 15:16:28 +0200

    Canvis a la crida dels controllers a les rutes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrganizerController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,19 +16,19 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', 'PageController@races');
-Route::get('curses/{race}', 'PageController@race')->name('race');
+Route::get('/', [PageController::class, 'races']);
+Route::get('curses/{race}', [PageController::class, 'race'])->name('race');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('races', 'Backend\RaceController')
         ->middleware('auth')
         ->middleware('admin')
         ->except('show');
 
-Route::get('organizers/register', 'OrganizerController@showRegisterForm')->name('registerorganizer');
+Route::get('organizers/register', [OrganizerController::class, 'showRegisterForm'])->name('registerorganizer');
 /*Route::get('/users', 'UserController@index');*/
 Route::post('/registerRunner', [UserController::class,'storeRunner'])->name('users.storeRunner');
 /*Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');*/
