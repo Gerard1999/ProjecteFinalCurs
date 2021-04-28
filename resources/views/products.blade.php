1452b5726d13f21374ec51c4acca16e8e87592f5
Gerard López <46751836+Gerard1999@users.noreply.github.com>
2021-04-28 23:41:16 +0200

    Estilitzant els productes

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="productes">
+<div class="contingut">
     <h1>Productes</h1>
-    @foreach($products as $product)
-    <div class="product">
-        <h4>{{$product->name}}</h4>
-        <img src="{{$product->get_image}}" alt="" height="200px">
+    <div class="productes">
+        @foreach($products as $product)
+        <div class="producte">
+            <h4>{{$product->name}}</h4>
+            <img src="{{$product->get_image}}" alt="" height="200px">
+            <p>{{$product->description}}</p>
+            <h4 class="price">{{$product->price}} €</h4>
+        </div>
+        @endforeach
     </div>
-    @endforeach
 </div>
 @endsection
