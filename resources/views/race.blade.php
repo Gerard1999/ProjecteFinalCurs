f666919cc0800db3082f1883a933c7073d88b34e
Gerard López <gerardlopo@gmail.com>
2021-05-10 21:19:32 +0200

    Dissenyant paàgina de curses individuals

--- resources/views/race.blade.php
+++ resources/views/race.blade.php
@@ -1,31 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                @if($race->img_cover)
-                    <img src="{{ $race->get_image }}" class="card-img-top img-cover">
-                @endif
-                <div class="card-body">
-                    <h3 class="card-title">{{$race->name}}</h3>
-                    <h5 class="card-text">Població: {{$race->location}}</h5>
-                    <p class="card-text">{{$race->description}}</p>
-                    <p class="text-muted mb-0">
-                        <em>
-                        Ho organitza:  {{ $race->organizer->user->name }}
-                        </em>
-                    </p>
-                    <h3>Modalitats:</h3>
-                    @foreach($race->categories as $category)
-                    <h5>{{$category->name_category}}</h5>
-
-                    @endforeach
-                    <em>{{ date('d/m/Y', strtotime($race->date)) }}</em>
-                </div>
-            </div>
+<div class="pagina-cursa">
+    <div class="header-imatge">
+        @if($race->img_cover)
+            <img src="{{ $race->get_image }}">
+        @endif
+        <div class="titular-cursa">
+            <h2>{{$race->name}}</h2>
         </div>
+    </div>
+    
+    <div class="info-cursa">
+        <h5 class="card-text">Població: {{$race->location}}</h5>
+        <p class="card-text">{{$race->description}}</p>
+        <p class="text-muted mb-0">
+            <em>
+            Ho organitza:  {{ $race->organizer->user->name }}
+            </em>
+        </p>
+        <h3>Modalitats:</h3>
+        @foreach($race->categories as $category)
+        <h5>{{$category->name_category}}</h5>
+
+        @endforeach
+        <em>{{ date('d/m/Y', strtotime($race->date)) }}</em>
     </div>
 </div>
 @endsection
