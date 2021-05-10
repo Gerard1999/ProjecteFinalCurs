1803f348a1a24eb5ad294f86c4800272d2ce3c3d
Gerard López <46751836+Gerard1999@users.noreply.github.com>
2021-05-11 00:28:10 +0200

    Estils pàgina cursa i curses

--- resources/views/race.blade.php
+++ resources/views/race.blade.php
@@ -12,19 +12,30 @@
     </div>
     
     <div class="info-cursa">
-        <h5 class="card-text">Població: {{$race->location}}</h5>
-        <p class="card-text">{{$race->description}}</p>
-        <p class="text-muted mb-0">
-            <em>
-            Ho organitza:  {{ $race->organizer->user->name }}
-            </em>
-        </p>
-        <h3>Modalitats:</h3>
-        @foreach($race->categories as $category)
-        <h5>{{$category->name_category}}</h5>
-
-        @endforeach
-        <em>{{ date('d/m/Y', strtotime($race->date)) }}</em>
+        <div class="dades-generals">
+            <h2 class="card-text">Població: {{$race->location}}</h2>
+            <b>{{ date('d/m/Y', strtotime($race->date)) }}</b>
+            <p class="card-text">{{$race->description}}</p>
+            <p class="text-muted mb-0">
+                <em>
+                Ho organitza:  {{ $race->organizer->user->name }}
+                </em>
+            </p>
+        </div>
+        <div class="modalitats">
+            @foreach($race->categories as $category)
+            <div class="modalitat">
+                <div class="modalitat-titol">
+                    <h3>{{$category->name_category}}</h3>
+                </div>
+                <div class="modalitat-info">
+                    <p>{{$category->kms}}km</p>
+                    <p>{{$category->elevation_gain}}m+</p>
+                    <p>{{$category->price}}€</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </div>
 </div>
 @endsection
