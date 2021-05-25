fc7051a619315bec6b51390dea877518ce362e93
Gerard López <gerardlopo@gmail.com>
2021-05-25 19:44:58 +0200

    Faltava un fragment de ruta al Kernel sobre els middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
-        'admin' => Middleware\IsAdmin::class,
-        'runner' => Middleware\IsRunner::class,
+        'admin' => \App\Http\Middleware\IsAdmin::class,
+        'runner' => \App\Http\Middleware\IsRunner::class,
     ];
 }
