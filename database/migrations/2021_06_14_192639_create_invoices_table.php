26c561f7d9a9db743097643c48ecd17edf127736
Gerard López <46751836+Gerard1999@users.noreply.github.com>
2021-06-15 10:07:37 +0200

    Revert "Revert "Creant taula invoices i rebut""
    This reverts commit bda0284ef9c35706460abff26a105cf416b37f95.

---
+++ database/migrations/2021_06_14_192639_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            
+            //Clau forana de Carro de la Compra
+            $table->unsignedBigInteger('shopping_cart_id')->nullable();
+            $table->foreign('shopping_cart_id')->references('id')->on('shopping_carts')->onDelete('cascade');
+
+            //Clau forana de User
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
--- resources/views/shoppingcart/index.blade.php
+++ resources/views/shoppingcart/index.blade.php
@@ -29,13 +29,13 @@
                         </td>
                         <td>{{$detail->product->price}}€</td>
                         <td class="comptador-preu">
-                        <form action="{{ route('updateCartDetail', $detail->id) }}" method="POST">
+                        <form action="{{ route('runner.updateCartDetail', $detail->id) }}" method="POST">
                             <input type="text" hidden value="sumar" name="accio">
                             <input type="submit" value="+" class="sumar boto boto-petit">
                             @csrf
                         </form>
                         <input id="qtt" type="number" min="1" value="{{$detail->quantity}}" disabled name="qtt">
-                        <form action="{{ route('updateCartDetail', $detail) }}" method="POST">
+                        <form action="{{ route('runner.updateCartDetail', $detail) }}" method="POST">
                             <input type="text" hidden value="restar" name="accio">
                             <input id="resta" type="submit" value="-" class="restar boto boto-petit">
                             @csrf
@@ -43,7 +43,7 @@
                         </td>
                         <td>{{$detail->sumPriceDetail()}}€</td>
                         <td>
-                            <form action="{{ route('deleteCartDetail', $detail) }}" method="POST">
+                            <form action="{{ route('runner.deleteCartDetail', $detail) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
                                 <input type="submit" value="Eliminar" class="boto boto-petit boto-vermell">
@@ -58,7 +58,11 @@
     @endif
     <div class="quadre-total">
         <h3>Total: {{$shopping_cart->priceCart()}}€</h3>
-        <a href="" class="boto boto-blau">Finalitzar Compra</a>
+        <form action="{{ route('runner.saveshopping', $shopping_cart) }}" method="POST">
+            @csrf
+            @method('POST')
+            <input type="submit" value="Finalitzar Compra" class="boto boto-blau" @if($shopping_cart->cartDetails->isEmpty()) disabled @endif>
+        </form>
     </div>
 </div>
 <script>
